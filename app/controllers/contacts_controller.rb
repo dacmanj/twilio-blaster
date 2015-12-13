2d50872e7e47718da9a0b87597a6116fce14d483
David Manuel <dmanuel@pflag.org>
2015-12-13 12:41:15 -0500

    add required params for group_membership

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -78,6 +78,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :email, :phone_number, :opt_in, :group_ids, :user_id)
+      params.require(:contact).permit(:first_name, :last_name, :email, :phone_number, :opt_in, :user_id, :group_ids=>[])
     end
 end
