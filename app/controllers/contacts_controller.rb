5902b92b5fa437e4ebe9fadc6367e7af04e5c217
David Manuel <dmanuel@pflag.org>
2015-12-21 00:10:06 -0500

    profile edit on main screen

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -44,7 +44,12 @@ class ContactsController < ApplicationController
   def update
     respond_to do |format|
       if @contact.update(contact_params)
-        format.html { redirect_to contacts_path, notice: 'Contact was successfully updated.' }
+        if URI(request.referer).path == '/'
+          redirect_path = URI(request.referer).path
+        else
+          redirect_path = contacts_path
+        end
+        format.html { redirect_to redirect_path, notice: 'Contact was successfully updated.' }
         format.json { render :show, status: :ok, location: @contact }
       else
         format.html { render :edit }
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,9 @@
 class VisitorsController < ApplicationController
+
+  def index
+    if user_signed_in?
+      @contact = current_user.contact
+    end
+  end
+
 end
