78b530f2e758c9862c36301b1da87abbc2eb656c
David Manuel, Jr <dmanuel@pflag.org>
2015-12-22 17:04:46 -0500

    multiple contacts -- default to first on main page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,9 @@ class VisitorsController < ApplicationController
 
   def index
     if user_signed_in?
-      @contact = current_user.contact
+      @contact = current_user.contacts.first
       authorize_action_for(@contact)
-      @message_logs = MessageLog.by_phone_number(current_user.contact.phone_number).order("created_at DESC").paginate(:page => params[:page])
+      @message_logs = MessageLog.by_phone_number(@contact.phone_number).order("created_at DESC").paginate(:page => params[:page])
       authorize_action_for(@message_logs)
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
 #  enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
-  has_one :contact
+  has_many :contacts
 
   def set_default_role
     if User.count == 0
