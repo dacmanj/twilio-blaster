bd7fcce82db7c0d8357e9747ccc616877356fa13
David Manuel <dmanuel@pflag.org>
2016-01-08 08:09:53 -0500

    skip twilio initializer for test environment

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,10 +1,12 @@
 
-@client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+if ["production","development"].include? Rails.env
+  @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
-@client.incoming_phone_numbers.list.each do |number|
-  if (number.phone_number ==  ENV['TWILIO_PHONE_NUMBER'])
-    number.update(
-    voice_url: "#{ENV['BASE_URL']}/twilio/inbound",
-    sms_url: "#{ENV['BASE_URL']}/twilio/inbound")
+  @client.incoming_phone_numbers.list.each do |number|
+    if (number.phone_number ==  ENV['TWILIO_PHONE_NUMBER'])
+      number.update(
+      voice_url: "#{ENV['BASE_URL']}/twilio/inbound",
+      sms_url: "#{ENV['BASE_URL']}/twilio/inbound")
+    end
   end
 end
